16a2ecd0d92ebc20160430d449d8c5e6c588ebdb
MAngelFM <88851006+MAngelFM@users.noreply.github.com>
2021-08-21 17:40:42 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+    min = int(input("Dame un numero de Minutos"))
+    seg = min * 60
+    cm = seg * 0.57
+    print("Centimetros recorridos ",cm)
     pass
 
 if __name__ == '__main__':
