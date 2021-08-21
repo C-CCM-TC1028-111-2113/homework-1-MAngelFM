e06e58441dfac054967454134840b0434a41db11
MAngelFM <88851006+MAngelFM@users.noreply.github.com>
2021-08-21 11:58:02 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-   pass
+    vid = int(input("Dame el numero de Juegos Nuevos "))
+    uss = int(input("Dame la cantidad de Juegos Usados "))
+
+    vid1 = vid*1000
+    uss1 = uss*350
+    tot = vid1 + uss1
+
+    print("El total es ",tot)
 
 
 
